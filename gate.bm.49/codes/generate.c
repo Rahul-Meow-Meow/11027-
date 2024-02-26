632864d9d0bfc49285ed6c8f78c50d4b4dc5f1dd
Rahul <rahulkpsbb@gmail.com>
2024-02-26 16:36:50 +0530

    1636_Feb-26

--- gate.bm.49/codes/generate.c
+++ gate.bm.49/codes/generate.c
@@ -12,15 +12,43 @@ int u(float t,int t_0){
 	ptr = (int *)realloc(ptr, sizeof(int)*(len+1));
 	*(ptr+len) = add;*/
 
+float f(float t){
+	if(t>=0 && t<=1){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+
+
+}
+
+float trap_integration(float a,float b,long int n){
+	printf("a, b =%f %f \n", a,b);
+	printf("f(a), f(b) = %f %f \n", f(a), f(b));
+	float h = (b-a)/(n);
+	float result = f(a) + f(b);
+	for (int i = 1; i<n; i++){ 
+		result += 2*(f(a+i*h));
+	}
+	printf("result_before_multi = %f" , result);
+	result = result * (h/2);
+	printf("result = %f\n\n",result);
+	return result;
+}
+
+
 void main(){
 	FILE *fptr;
 	fptr = fopen("Values.txt","w");
 	//int *ptr = (int*)malloc(int(s))
 	float t = -3;
-	for(int i = 0; i <=1000 ; i++){
-		t = t+ (1/100.0);
+	for(int i = 0; i <=100 ; i++){
+		t = t+ (1/10.0);
 		//printf("%f ,", t);
-		float value = t*u(t,0) + (t-2)*u(t,2) -2*(t-1)*u(t,1);
-		fprintf(fptr, "%.3f %.3f\n", t, value);
+		float value1 = trap_integration(t-1,t,100000);
+		float value2 = t*u(t,0) + (t-2)*u(t,2) -2*(t-1)*u(t,1);
+		fprintf(fptr, "%.3f %.6f %.6f\n", t, value1, value2);
 	}
+	fclose(fptr);
 }
--- gate.bm.49/codes/plot.py
+++ gate.bm.49/codes/plot.py
@@ -5,8 +5,9 @@ x_values = data[:,0]
 
 y_values = data[:,1]
 
+y_calcu = data[:,2]
 plt.stem(x_values, y_values, label='y(t) v/s t')
-
+plt.scatter(x_values, y_calcu, marker = 'X',s=100, color = 'red', label = r'Simulation')
 plt.xlabel('t')
 plt.ylabel('y(t)')
 
